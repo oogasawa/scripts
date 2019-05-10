30b11701a5073b50d544b5e8b1fd50696fd967d3
Osamu Ogasawara <oogasawa@nig.ac.jp>
2019-05-10 21:13:03 +0900

    Add ubuntu_installNode.sh

---
+++ ubuntu_installNode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# How to install Node.js 10 on Ubuntu 18.04 LTS
+# https://joshtronic.com/2018/05/08/how-to-install-nodejs-10-on-ubuntu-1804-lts/
+
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install nodejs
+
+
+
