9aba177ae72a00f75371a0c5dab1ccc832b20454
Osamu Ogasawara <osamu.ogasawara@gmail.com>
2020-11-11 23:56:21 +0900

    Remove old scripts

--- activate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-function text_escapeHtml() {
-	java -cp $HOME/works02/simplejlib-util/target/simplejlib-util-1.3.0-jar-with-dependencies.jar \
-		 net.ogalab.simplejlib.util.text.HtmlEncoder
-}
--- scripts_chmod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ~/scripts.0
-chmod +x *.sh *.js *.mjs *.pl *.py 2> /dev/null
-
-cd ~/scripts
-chmod +x *.sh *.js *.mjs *.pl *.py 2> /dev/null
-
